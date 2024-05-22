bddfae27df3fb6510306e5a546816a89995154d2
davifox183 <davifox@hotmail.com>
2024-05-22 19:13:45 -0300

    first commit

---
+++ src/home/index.tsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import { Container ,BigText } from './styles'; // Importando o componente estilizado
+
+export function Texto() {
+  return (
+        <Container>
+      <BigText>Funcionou!</BigText>
+      </Container>
+  );
+}
+
+
---
+++ src/home/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+
+export const Container = styled.View`
+    flex:1;
+    background-color: "#000";
+    align-items: center;
+    justify-items: center;
+    margin-top: 300px;
+`;
+
+export const BigText = styled.Text`
+    font-size: 50px;
+
+`
